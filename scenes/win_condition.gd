353dc62b84ad4d35269e4ba811edea44e3f991f6
RBW1999 <rabe.wortmann@gmail.com>
2026-07-26 18:39:42 +0200

    feat: add level 4

--- scenes/win_condition.gd
+++ scenes/win_condition.gd
@@ -18,3 +18,8 @@ func _ready() -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if(body == target):
 		done = true
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if(body == target):
+		done = false
